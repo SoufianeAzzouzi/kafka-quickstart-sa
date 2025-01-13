e1556763b6b2778207dfc817c7000bcd92d8f440
SoufianeAzzouzi <60263640+SoufianeAzzouzi@users.noreply.github.com>
2025-01-13 17:55:17 +0100

    Add logger to print received messages

--- src/main/java/com/kodva/kafkademo/consumer/MessageConsumer.java
+++ src/main/java/com/kodva/kafkademo/consumer/MessageConsumer.java
@@ -1,13 +1,18 @@
 package com.kodva.kafkademo.consumer;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageConsumer {
 
+    private static final Logger logger = LoggerFactory.getLogger(MessageConsumer.class);
+
     @KafkaListener(topics = "my-topic", groupId = "${spring.kafka.consumer.group-id}")
     public void listen(String message) {
-        System.out.println(" -> " + message);
+        logger.info(String.format("Message received : %s", message));
     }
 }
