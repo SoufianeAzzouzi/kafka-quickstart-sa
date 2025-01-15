067f603e253af6cb1e4904299fa8a046e0dd5f81
SoufianeAzzouzi <60263640+SoufianeAzzouzi@users.noreply.github.com>
2025-01-15 20:27:06 +0100

    Add comments to explain the components

--- src/main/java/com/kodva/kafkademo/consumer/MessageConsumer.java
+++ src/main/java/com/kodva/kafkademo/consumer/MessageConsumer.java
@@ -11,6 +11,12 @@ public class MessageConsumer {
 
     private static final Logger logger = LoggerFactory.getLogger(MessageConsumer.class);
 
+    /**
+     * Here, we specify the topic from which we will retrieve our message,
+     * as well as the consumer group ID to which our consumer belongs
+     *
+     * @param message : consumed message, that was sent by the producer
+     */
     @KafkaListener(topics = "my-topic", groupId = "${spring.kafka.consumer.group-id}")
     public void listen(String message) {
         logger.info(String.format("Message received : %s", message));
--- src/main/java/com/kodva/kafkademo/producer/MessageProducer.java
+++ src/main/java/com/kodva/kafkademo/producer/MessageProducer.java
@@ -10,6 +10,13 @@ public class MessageProducer {
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
+    /**
+     * Here, we pass the topic and the message (which we received from the controller)
+     * to the Kafka template's send method in order to send them to the broker.
+     *
+     * @param topic     : destination topic
+     * @param message   : message to store in the topic
+     */
     public void sendMessage(String topic, String message){
         kafkaTemplate.send(topic, message);
     }
